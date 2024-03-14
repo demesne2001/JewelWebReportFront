513e7c1882fd697ad57409dbce888d7c974cc9a6
Jaimin H Jungi <jj@gmail.com>
2024-03-14 13:01:15 +0530

    Navbar Completed without close

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Navbar from './Component/Sales-Efficiency-Analysis-Dashboard/NavigationBar/Navbar';
+import Dashboard from './Component/Sales-Efficiency-Analysis-Dashboard/Dashboard/Dashboard';
+
+import './Component/Assets/css/bootstrap-select.min.css'
+import './Component/Assets/css/bootstrap.min.css'
+import './Component/Assets/css/charts.min.css'
+import './Component/Assets/css/font-awesome-all.min.css'
+import './Component/Assets/css/reset.css'
+import './Component/Assets/css/style.css'
+
+// import bootstrapSelectMin from './Component/Assets/js/bootstrap-select.min';
+// import bootstrapMin from './Component/Assets/js/bootstrap.min'
+// import jqueryMigrate from './Component/Assets/js/jquery-migrate';
+// import jquaryMin from './Component/Assets/js/jquery.min'
+// import main from './Component/Assets/js/main'
+// import popperMin from './Component/Assets/js/popper.min';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> 
+      <div>
+          <Navbar/>
+
+          <Routes>
+            <Route exact path='/' element={<Dashboard/>}></Route>
+            </Routes>
+
+            
+            {/* <script type="text/javascript" src={bootstrapSelectMin}></script>
+            <script type="text/javascript" src={bootstrapMin}></script>
+            <script type="text/javascript" src={jqueryMigrate}></script>
+            <script type="text/javascript" src={jquaryMin}></script>
+            <script type="text/javascript" src={main}></script>
+            <script type="text/javascript" src={popperMin}></script> */}
+
+        
+      </div>
+    </Router>
+
   );
 }
 
---
+++ src/Component/Sales-Efficiency-Analysis-Dashboard/Dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Inside Dashboard </h1>
+    </div>
+  )
+}
