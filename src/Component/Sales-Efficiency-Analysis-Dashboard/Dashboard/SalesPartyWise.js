a66d21e832ed30aef7f5d4e233912f400370aa1b
Jaimin H Jungi <jj@gmail.com>
2024-03-15 13:48:40 +0530

    before cylinder

--- src/Component/Sales-Efficiency-Analysis-Dashboard/Dashboard/SalesPartyWise.js
+++ src/Component/Sales-Efficiency-Analysis-Dashboard/Dashboard/SalesPartyWise.js
@@ -1,8 +1,56 @@
 import React from 'react'
 
-import salespartywisepng from '../../Assets/image/salesparty wise.png'
+import ReactApexChart from 'react-apexcharts';
 
 export default function SalesPartyWise() {
+	const series = [{
+		name: 'Marine Sprite',
+		data: [44, 55, 41, 37, 22, 43, 21]
+	  }]
+
+	const options = {
+		chart: {
+		  type: 'bar',
+		  height: 350,
+		  stacked: true,
+		  stackType: '100%'
+		},
+		plotOptions: {
+		  bar: {
+			horizontal: true,
+			 dataLabels: {
+			 enabled: true,
+			 position: 'top'
+				}
+		  },
+		},
+		stroke: {
+		  width: 1,
+		  colors: ['#fff']
+		},
+		title: {
+		  text: ''
+		},
+		xaxis: {
+		  categories: [2008, 2009, 2010, 2011, 2012, 2013, 2014],
+		},
+		tooltip: {
+		  y: {
+			formatter: function (val) {
+			  return val + "K"
+			}
+		  }
+		},
+		fill: {
+		  opacity: 1
+		
+		},
+		legend: {
+		  position: 'top',
+		  horizontalAlign: 'left',
+		  offsetX: 40
+		}
+	  }
   return (
     <div className="col-lg-4 col-md-6 col-12">
 								<div className="graph-card">
@@ -12,7 +60,7 @@ export default function SalesPartyWise() {
 										<i className="fas fa-external-link-alt"></i>
 									</a>
 									<div className="crancy-progress-card card-contain-graph">
-										<img src={salespartywisepng}/>
+									<ReactApexChart options={options} series={series} type="bar" height={350}/>
 									</div>
 								</div>
 							</div>
--- src/Component/Sales-Efficiency-Analysis-Dashboard/Dashboard/YearWise.js
+++ src/Component/Sales-Efficiency-Analysis-Dashboard/Dashboard/YearWise.js
@@ -5,66 +5,136 @@ import ReactApexChart from 'react-apexcharts';
 export default function YearWise() {
 	
           
-		const series =  [{
-		  name: 'PRODUCT A',
-		  data: [44, 55, 41, 67, 22, 43]
-		}, {
-		  name: 'PRODUCT B',
-		  data: [13, 23, 20, 8, 13, 27]
-		}]
-		
-		const options = {
-		 colors:['#26e7a5','#7d64d1'],
-		  chart: {
-			type: 'bar',
-			height: 350,
-			stacked: true,
-			toolbar: {
-				show: false,
-			},
-			zoom: {
-			  enabled: true
-			}
-		  },
-		  responsive: [{
-			breakpoint: 480,
-			options: {
-			  legend: {
-				position: 'bottom',
-				offsetX: -10,
-				offsetY: 0
-			  }
-			}
-		  }],
-		  plotOptions: {
-			bar: {
-			  horizontal: false,
-			  borderRadius: 0,
-			  dataLabels: {
-				total: {
-				  enabled: true,
-				  style: {
-					fontSize: '13px',
-					fontWeight: 900
-				  }
+	const series = [
+		{
+		  name: 'Actual',
+		  data: [
+			{
+			  x: '2011',
+			  y: 1292,
+			  goals: [
+				{
+				  name: 'Expected',
+				  value: 1400,
+				  strokeHeight: 5,
+				  strokeColor: '#775DD0'
 				}
-			  }
+			  ]
 			},
-		  },
-		  xaxis: {
-			type: 'datetime',
-			categories: ['01/01/2011 GMT', '01/02/2011 GMT', '01/03/2011 GMT', '01/04/2011 GMT',
-			  '01/05/2011 GMT', '01/06/2011 GMT'
-			],
-		  },
-		  legend: {
-			position: 'right',
-			offsetY: 40
-		  },
-		  fill: {
-			opacity: 1
+			{
+			  x: '2012',
+			  y: 4432,
+			  goals: [
+				{
+				  name: 'Expected',
+				  value: 5400,
+				  strokeHeight: 5,
+				  strokeColor: '#775DD0'
+				}
+			  ]
+			},
+			{
+			  x: '2013',
+			  y: 5423,
+			  goals: [
+				{
+				  name: 'Expected',
+				  value: 5200,
+				  strokeHeight: 5,
+				  strokeColor: '#775DD0'
+				}
+			  ]
+			},
+			{
+			  x: '2014',
+			  y: 6653,
+			  goals: [
+				{
+				  name: 'Expected',
+				  value: 6500,
+				  strokeHeight: 5,
+				  strokeColor: '#775DD0'
+				}
+			  ]
+			},
+			{
+			  x: '2015',
+			  y: 8133,
+			  goals: [
+				{
+				  name: 'Expected',
+				  value: 6600,
+				  strokeHeight: 13,
+				  strokeWidth: 0,
+				  strokeLineCap: 'round',
+				  strokeColor: '#775DD0'
+				}
+			  ]
+			},
+			{
+			  x: '2016',
+			  y: 7132,
+			  goals: [
+				{
+				  name: 'Expected',
+				  value: 7500,
+				  strokeHeight: 5,
+				  strokeColor: '#775DD0'
+				}
+			  ]
+			},
+			{
+			  x: '2017',
+			  y: 7332,
+			  goals: [
+				{
+				  name: 'Expected',
+				  value: 8700,
+				  strokeHeight: 5,
+				  strokeColor: '#775DD0'
+				}
+			  ]
+			},
+			{
+			  x: '2018',
+			  y: 6553,
+			  goals: [
+				{
+				  name: 'Expected',
+				  value: 7300,
+				  strokeHeight: 2,
+				  strokeDashArray: 2,
+				  strokeColor: '#775DD0'
+				}
+			  ]
+			}
+		  ]
+		}
+	  ]
+	  const options = {
+		chart: {
+		  height: 350,
+		  type: 'bar'
+		},
+		plotOptions: {
+		  bar: {
+			columnWidth: '60%'
+		  }
+		},
+		colors: ['#00E396'],
+		dataLabels: {
+		  enabled: false
+		},
+		legend: {
+		  show: true,
+		  showForSingleSeries: true,
+		  customLegendItems: ['Actual', 'Expected'],
+		  markers: {
+			fillColors: ['#00E396', '#775DD0']
 		  }
 		}
+	  }
+		
   return (
     <div className="col-lg-4 col-md-6 col-12">
 								<div className="graph-card">
